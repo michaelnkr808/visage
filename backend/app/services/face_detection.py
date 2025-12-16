8955942d623f633fe80a4e48e2182188cfd23b35
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-12-15 17:37:17 -0800

    Add rest of backend

--- backend/app/services/face_detection.py
+++ backend/app/services/face_detection.py
@@ -1,7 +1,10 @@
-import cv2 as cv
 import __future__ as print_function
 import argparse
 import numpy as np
+import io
+from deepface import DeepFace
+import cv2 as cv
+from PIL import Image
 from models.face_scan import Photo
 from services.database import get_photo_by_id, get_most_recent_photo
 
@@ -37,25 +40,7 @@ photo_array = cv.imdecode(buf, cv.IMREAD_GRAYSCALE)
 if photo_array is None:
     print(f"Error: could not load photo from {Photo}")
 else:
-    def detectAndDisplay(frame):
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        frame_gray = cv.equalizeHist(frame_gray)
+    detectAndDisplay(photo_array)
 
-        #detect faces
-        faces = face_cascade.detectMultiScale(frame_gray)
-        for (x,y,w,h) in faces:
-            center = (x + w//2 y + h//2)
-            frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360 (255, 0, 255), 4)
-
-            faceROI = frame_gray[y:y+h,x:x+w]
-            #detect eyes in each face
-            eyes = eyes_cascade.detectMultiScale(faceROI)
-            for (x2,y2,w2,h2) in eyes:
-                eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-                radius = int(round((w2 + h2)*0.25))
-                frame = cv.circle(frame, eye_center, radius, (255, 0, 0), 4)
-            
-        cv.imshow('Capture - Face Detection', frame)
-   
 
 
